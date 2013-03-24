9bb3b9db1c7a63150ff287faebcfde7131f45b15
Tobi <tobias.baur@gmail.com>
2013-03-24 16:50:16 +0000

    Machine Learning for Hackers Intro Chapter R Script

---
+++ mlfh/mlfh1.R
@@ -0,0 +1,7 @@
+### Machine Learning for Hackers - very first tutorial
+### https://docs.google.com/file/d/0B4ZqvMfne52dSTk3R0tDTG1HZjA/edit
+
+#install and load ggplot2
+setwd ("~/.vim/")
+install.packages("ggplot2", dependencies = TRUE)
+library(ggplot2)
