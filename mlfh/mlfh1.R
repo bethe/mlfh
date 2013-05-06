3c9573eeb02e33a330ee4e05db52b8d29f06d218
bethe <tobias.baur@gmail.com>
2013-05-06 11:05:04 +0000

    ch01 half way

--- mlfh/mlfh1.R
+++ mlfh/mlfh1.R
@@ -1,9 +1,11 @@
 ### Machine Learning for Hackers - very first tutorial
 ### https://docs.google.com/file/d/0B4ZqvMfne52dSTk3R0tDTG1HZjA/edit
 
-#install and load ggplot2
-setwd ("~/.vim/")
-install.packages("ggplot2", dependencies = TRUE)
+# load ggplot2
 library(ggplot2)
 
-# new comment
+# set working directory
+setwd("~/mlfh/mlfh/mlfh/ch01")
+
+# Load data (file at http://www.dropbox.com/s/xt4k3851)
+ufo <- read.delim(, sep="\t", stringsAsFactors=FALSE, header=FALSE, na.strings="")
